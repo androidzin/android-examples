7913eabe51b79e70d119ea2b8b8fb7f48b0aacc3
brunomateus <brunomateus@gmail.com>
2013-04-19 10:14:40 -0300

    Initial commit. Custom Spinner with icon

---
+++ src/br/com/androidzin/android_examples/SpinnerIconActivity.java
@@ -0,0 +1,47 @@
+package br.com.androidzin.android_examples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.widget.SpinnerAdapter;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+
+public class SpinnerIconActivity extends SherlockActivity implements OnNavigationListener{
+
+	private SpinnerAdapter mSpinnerAdapter;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_spinner_icon);
+		
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+		
+		
+		List<SpinnerRow> options = new ArrayList<SpinnerRow>();
+		options.add(new SpinnerRow("Droid 1", R.drawable.android01));
+		options.add(new SpinnerRow("Droid 2", R.drawable.android02));
+		options.add(new SpinnerRow("Droid 3", R.drawable.android03));
+		mSpinnerAdapter  = new MySpinnerAdapter(getApplicationContext(),
+				R.layout.spinner_row, R.id.text, options);
+		
+		actionBar.setListNavigationCallbacks(mSpinnerAdapter, this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+		return false;
+	}
+
+}
